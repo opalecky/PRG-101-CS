c06fc77ac43a33a5ca0c5dab63b08fffe9ac6426
opalecky <adam@opalecky.cz>
2022-11-17 12:26:22 +0100

    Apple and inheritance / abstraction

---
+++ ClassTesting/Apple.cs
@@ -0,0 +1,42 @@
+﻿namespace OPP_Showcase
+{
+
+    public class Fruit
+    {
+        public string taste;
+    }
+
+    public class Apple : Fruit
+    {
+        public enum TYPES
+        {
+            GRANNY_SMITH    = 0x00,
+            RED_DELICIOUS   = 0xFF  // 1111 1111 (16 × 16^1) + (16 × 16^0) | 15×16=240 + 15 | 0 1 2 3 4 5 6 7 8 9 A (10) B (11) C (12) D (13) E (14) F (15)
+                                    // (1 × 2^7) + (1 × 2^6) + (1 × 2^5) + (1 × 2^4) + (1 × 2^3) + (1 × 2^2) + (1 × 2^1) + (1 × 2^0)
+        }
+        int age; // member variable
+        Apple.TYPES type;
+
+        public static void callApple()
+        {
+            Console.WriteLine("Hello this is apple support.");
+        }
+
+        public Apple(int inAge, Apple.TYPES inType, string inTaste)
+        {
+
+            age = inAge;
+            type = inType;
+            taste = inTaste;
+        }
+
+        public int prt() // ptr = print
+        {
+            Console.Write(age); // vypsání
+            return age; // vrácení
+        }
+    }
+
+}
+
+
---
+++ ClassTesting/Program.cs
@@ -0,0 +1,36 @@
+﻿// Class [Třída] - Definice struktury
+
+/*
+ * ENUM - Vyčíslení = Přiřazení hodnoty ke jménu v tabulce paměti
+ * 
+ * 3 základní koncepty OOP (objektově orientované programování):
+ * Abstrakce (Enkapsulace) - Třída je jenom černá schránka, která si dělá svoje (Abstrakce), 
+ *                           Enkapsulace (viditelnost)
+ *                            - Private = Vidí jenom ta jediná třída (nejvyšší úroveň abstrakce) - Můj mozek ví, jakou kyselinu mám v žaludku, ale neříká mi to
+ *                            - Protected = Vidí třídy a jejich podtřídy - Můj mozek ví, kde mám ruku a říká mi to
+ *                            - Public = Vidí všichni - Všichni kdo chtějí věět kde mám ruku, to můžou zjistit
+ * Inheritance (dědičnost) - Atributy se předávají z nadtřídy do podtřídy (superclass -> subclass)
+ * Polymorfismus
+ * 
+ * 
+ * [Tabulka Paměti - [[string - 8B]][[pointer - 8B]]]   [Jméno třídy] -> [Pointer]
+ * [Definice třídy 36B - [Deklarace proměných - int -4B, int-4B, string-12B][Statické Proměnné][Statické Funkce]][Volné místo 4B][Instance - 20B][Instance - 20B][Instance - 20B]
+ * 
+ */
+
+
+using OPP_Showcase;
+using System;
+
+class main
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("RUN");
+        Apple apple1 = new Apple(10, Apple.TYPES.GRANNY_SMITH, "sweet");
+        Apple apple2 = new Apple(20, Apple.TYPES.GRANNY_SMITH, "bitter");
+        Console.WriteLine(apple2.taste);
+        Console.WriteLine(apple1.prt());
+        Console.WriteLine(apple2.prt());
+    }
+}
